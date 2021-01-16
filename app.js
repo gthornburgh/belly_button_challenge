93d27427cf659efbed2e222764caee2aaa57010c
gthornburgh <grantthornburgh@gmail.com>
2021-01-16 16:18:14 -0500

    commit 8

--- app.js
+++ app.js
@@ -2,3 +2,10 @@
 
 function getPlots(id) {
 
+    // samples json
+        d3.json("samples.json").then (data =>{
+            var samples = data.samples;
+            var result = samples.filter(sample => sample.id == id)[0]
+            var ids = result.otu_ids;
+            var sampleValues =  result.sample_values.slice(0,10).reverse();
+            var labels =  result.otu_labels.slice(0,10).reverse();
