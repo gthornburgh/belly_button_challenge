ae0f374a4c8ffefc341e2ce2dab08e6304d035c3
gthornburgh <grantthornburgh@gmail.com>
2021-01-16 16:36:57 -0500

    commit 18

--- app.js
+++ app.js
@@ -119,3 +119,7 @@ function getDemoInfo(id) {
 // get data from json
 d3.json("samples.json").then((data)=> {
     var metadata = data.metadata;
+    console.log(metadata)
+    var result = metadata.filter(meta => meta.id.toString() === id)[0];
+    var demographicInfo = d3.select("#sample-metadata");
+    demographicInfo.html("");
